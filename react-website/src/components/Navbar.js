d8409f599f2dbd53aca2728a84dc9aa7b418a15b
Raveen Panditha <raveenpanditha9@gmail.com>
2021-07-29 14:30:23 +0530

    changes

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+       <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navbar-logo">
+                    TRVL
+
+                </Link>
+
+            </div>
+        </nav>
+
+       </>
     )
 }
 
