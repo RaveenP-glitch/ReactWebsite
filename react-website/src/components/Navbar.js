9912bd801da0b584fe323e1ef0f2d495fdf8d590
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-11-08 23:14:53 +0530

    addcss

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -6,6 +6,10 @@ function Navbar() {
 
     const [click, setClick] = useState(false);
 
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    
+
     return (
        <>
         <nav className="navbar">
@@ -13,11 +17,34 @@ function Navbar() {
                 <Link to="/" className="navbar-logo">
                     <b className='logotitle'>LODIYA_LABS</b> <i className='fab fa-typo3 fa-3x'/>
                 </Link>
-                <div className='menu-icon'>
+                <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
 
 
                 </div>
+
+                <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
 
             </div>
         </nav>
